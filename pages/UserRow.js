a584a931ab0beaeb649dee990c1733cfbecdbc13
rebeccapeltz <rebeccapeltz@gmail.com>
2023-04-30 17:54:16 -0700

    add check for user because of build check

--- pages/UserRow.js
+++ pages/UserRow.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function UserRow(props) {
-debugger
-const { id, name,  email } = props.user;
-  return (
-        <tr>
-            <td>{id}</td>
-            <td>{name}</td>
-            <td>{email}</td>
-        </tr>
-  )
+  // debugger
+  // const { id, name,  email } = props.user;
+  debugger;
+  if (props.user && props.user.id) {
+    const { id, name, email } = props.user;
+    return (
+      <tr>
+        <td>{id}</td>
+        <td>{name}</td>
+        <td>{email}</td>
+      </tr>
+    );
+  } else {
+    return <p>no data</p>;
+  }
 }
